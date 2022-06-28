1cbe5cb5f69cd4fb6e6251965c5735ba38f8f639
Adhithyan <65805796+adhithyanmv@users.noreply.github.com>
2022-06-28 19:53:59 +0530

    Add files via upload

---
+++ portApp/urls.py
@@ -0,0 +1,17 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path("", views.index),
+    path("index.html", views.index),
+    path("project1", views.project1),
+    path("project2", views.project2),
+    path("project3", views.project3),
+    path("project4", views.project4),
+    path("project5", views.project5),
+    path("project6", views.project6),
+    path("project7", views.project7),
+    path("project8", views.project8),
+    path("project9", views.project9),
+]
---
+++ portApp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def project1(request):
+    return render(request, 'portfolio-details1.html')
+
+def project2(request):
+    return render(request, 'portfolio-details2.html')
+
+def project3(request):
+    return render(request, 'portfolio-details3.html')
+
+def project4(request):
+    return render(request, 'portfolio-details4.html')
+
+def project5(request):
+    return render(request, 'portfolio-details5.html')
+
+def project6(request):
+    return render(request, 'portfolio-details6.html')
+
+def project7(request):
+    return render(request, 'portfolio-details7.html')
+
+def project8(request):
+    return render(request, 'portfolio-details8.html')
+
+def project9(request):
+    return render(request, 'portfolio-details9.html')
+
